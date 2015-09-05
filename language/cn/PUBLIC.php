960cb94896d6a372880280637d84275ae35fc274
goodplayer <woshiyigelaji@gmail.com>
2015-09-05 12:29:06 +0800

    c

--- language/cn/PUBLIC.php
+++ language/cn/PUBLIC.php
@@ -1,7 +1,7 @@
 <?php
 
 // Translated into English by QwataKayean . All rights reversed (C) 2012
-// 2Moons - Copyright (C) 2010-2012 J�n Kropke
+// 2Moons - Copyright (C) 2010-2012 Jän Kropke
 
 // Site Title
 $LNG['siteTitleIndex']				= 'Index';
@@ -96,7 +96,7 @@ $LNG['registerSendComplete']		= 'Thank you for the registration. You must check 
 
 $LNG['registerWelcomePMSenderName']	= 'Administrator';
 $LNG['registerWelcomePMSubject']	= 'Welcome';
-$LNG['registerWelcomePMText']		= 'Welcome to %s! First build a solar power plant,because energy is needed for the production of raw materials. To build one, left click in the menu on "buildings". Then build the 4th Building from the top. </br> When you have energy, you can begin to build mines. Go to buildings on the menu and build a metal mine, followed by a crystal mine. </br></br>In order to be able to build ships you need to have a shipyard. To see what is needed to unlock that building you can take a look at "Technologies" at the left menu.</br>If you more questions you can look at the beginnersguide, our forum or open a support ticket. </br></br> The team wishes you much fun exploring the universe!';
+$LNG['registerWelcomePMText']		= '欢迎你 %s! 首先建造一个太阳能发电厂。生产原材料需要能源。 To build one, left click in the menu on "buildings". Then build the 4th Building from the top. </br> When you have energy, you can begin to build mines. Go to buildings on the menu and build a metal mine, followed by a crystal mine. </br></br>In order to be able to build ships you need to have a shipyard. To see what is needed to unlock that building you can take a look at "Technologies" at the left menu.</br>If you more questions you can look at the beginnersguide, our forum or open a support ticket. </br></br> The team wishes you much fun exploring the universe!';
 
 //Vertify
 
