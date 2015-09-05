4e2c2ad03087f3d3cd7075b1a393d9c35d981df2
goodplayer <woshiyigelaji@gmail.com>
2015-09-05 11:22:02 +0800

    c

---
+++ language/zh_CN/BANNER.php
@@ -0,0 +1,17 @@
+<?php
+
+ 
+// Translated into English by QwataKayean . All rights reversed (C) 2012
+// 2Moons - Copyright (C) 2010-2012 Slaver
+
+
+ 
+$LNG['ub_points']		= 'Points';
+$LNG['ub_fleets']		= '舰队';
+$LNG['ub_researh']		= '研究';
+$LNG['ub_buildings']	= '建筑';
+$LNG['ub_defenses']		= '防御';
+
+$LNG['ub_fights']		= 'Battles';
+$LNG['ub_quote']		= 'Quote';
+$LNG['ub_rank']			= 'Position';
